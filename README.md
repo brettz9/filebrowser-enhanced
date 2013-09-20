edeb1a94f7a7239fcf9ff8221eaa927439247201
Brett Zamir <brettz9@yahoo.com>
2013-09-19 21:28:03 -0700

    autocomplete for paths and autofocus on this autocomplete

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Overlays Firefox's built-in desktop browser with additional functionality.
 Currently is limited to:
 
 1. Placing the current directory as a native path within a textbox (instead of showing the file: path as text).
-1. Rather than needing to copy or change the file path within the browser's location bar, one may also copy-paste or change the native path within the page-internal textbox.
+1. Rather than needing to copy or change the file path within the browser's location bar, one may also copy-paste or change the native path within the page-internal textbox (which is capable of path autocomplete).
 1. Adds a folder icon which can be clicked to reveal the current directory on the native OS desktop (e.g., open the currently browsed directory within Windows Explorer). A folder icon with the same functionality is also present on the add-on bar when browsing to a file: URL (e.g., when viewing a .txt file in Firefox), allowing one to jump to the containing folder on one's desktop.
--- data/directoryMod.js
+++ data/directoryMod.js
@@ -1,5 +1,10 @@
 /*globals self, jml*/
-(function () {'use strict';
+(function () {
+    'use strict';
+
+function l(msg) {
+    console.log(msg);
+}
 
 // self.port.emit(name, jsonSerializableData);
 // self.port.on/once/removeListener(name, function () {}); // self.on is used instead for built-in message listening
@@ -13,28 +18,60 @@ if (window.location.href.indexOf('.') === -1) { // In a directory (regex should 
     on('getFileURLFromNativePathResponse', function (fileURL) {
         window.location.href = fileURL;
     });
+    on('autocompleteValuesResponse', function (data) {
+        var datalist = document.getElementById(data.listID);
+        while (datalist.firstChild) {
+            datalist.removeChild(datalist.firstChild);
+        }
+        data.optValues.forEach(function (optValue) {
+            var option = jml('option', {
+                // text: optValue,
+                value: optValue
+            });
+            datalist.appendChild(option);
+        });
+    });
     on('getNativePathFromFileURLResponse', function (result) {
         var h1 = document.querySelector('h1');
-        h1.replaceChild(document.createTextNode('Index of '), h1.firstChild);
+        while (h1.firstChild) {
+            h1.removeChild(h1.firstChild);
+        }
+        h1.appendChild(document.createTextNode('Index of '));
         h1.appendChild(jml(
-            'input', {type: 'text', size: 100, value: result, $on: {change: function (e) {
-                on('pathExistsResponse', function (pathExists) {
-                    if (pathExists) {
-                        emit('getFileURLFromNativePath', e.target.value);
+            'input', {
+                type: 'text', id: 'pathBox', list: 'datalist', autocomplete: 'off', autofocus: 'autofocus',
+                size: 100, value: result,
+                $on: {
+                    change: function (e) {
+                        on('pathExistsResponse', function (pathExists) {
+                            if (pathExists) {
+                                emit('getFileURLFromNativePath', e.target.value);
+                            }
+                        });
+                        emit('pathExists', e.target.value);
+                    },
+                    input: function (e) {
+                        emit('autocompleteValues', {
+                            value: e.target.value,
+                            listID: e.target.getAttribute('list')
+                        });
                     }
-                });
-                emit('pathExists', e.target.value);
-            }}},
+                }
+            },
+            'datalist', {id: 'datalist'},
             'input', {
                     type: 'button',
                     style: 'border: none; margin-left: 5px; background-color: transparent; width: 25px; background-repeat: no-repeat; '+
                             'background-size: 20px 20px; '+
                             'background-image: url("' + options.folderImage + '");',
-                    $on: {click: function (e) {
+                    $on: {click: function () {
                         emit('reveal', result);
                     }}
                 }, null
         ));
+        var input = document.querySelector('#pathBox');
+        input.focus();
+        input.setSelectionRange(input.value.length, input.value.length);
     });
     emit('getNativePathFromFileURL', document.baseURI);
 
