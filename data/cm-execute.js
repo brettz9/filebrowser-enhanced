99f3b72bbfb42e15a358006778eb9aaee44f1de7
Brett Zamir <brettz9@yahoo.com>
2014-04-10 22:28:43 +0800

    Prevent execute from showing on folders

--- data/cm-execute.js
+++ data/cm-execute.js
@@ -2,7 +2,8 @@
 (function () {'use strict';
 
 self.on('context', function (node) {
-    return (/^file:/).test(node.href);
+    var href = node.href;
+    return (/^file:/).test(href) && href.slice(-1) !== '/'; // No need on directories with reveal option
 });
 
 self.on('click', function (node, data) {
