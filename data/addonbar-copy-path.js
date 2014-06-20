086c7daeebab06bbfb05f8e3ac699b6034ab93c5
Brett Zamir <brettz9@yahoo.com>
2014-06-20 11:02:02 +0800

    Change copy-path into two separate buttons, one to copy path and another for parent directory; JSLint; use more human-readable APIs; hide parent button if at root

--- data/addonbar-copy-path.js
+++
@@ -1,9 +0,0 @@
-/*globals self */
-window.addEventListener('click', function(e) {'use strict';
-
-    if (e.button === 2 || (e.button === 0 && e.shiftKey === true)) {
-        self.port.emit('copyDirectoryPath');
-        e.preventDefault();
-    }
-
-}, true);
